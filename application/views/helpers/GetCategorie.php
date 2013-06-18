aea53798c2b76b87dce287d4b1d484c9d0bd159e
webmaster <webmaster@L05MAC14.local>
2013-06-18 21:26:06 +0200

    les1

--- application/views/helpers/GetCategorie.php
+++ application/views/helpers/GetCategorie.php
@@ -8,7 +8,7 @@ class Zend_View_Helper_GetCategorie extends Zend_View_Helper_Abstract
 {
 		
         public function GetCategorie($productId){
-            $productcategorieModel = new Application_Model_ProductCategorie();
+            $productcategorieModel = new Application_Model_Productcategorie();
             $categorie = $productcategorieModel->getCategorieForProduct((int)$productId);
             if (!empty($categorie)) {
                 foreach ($categorie as $c) {
